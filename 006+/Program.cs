719b7c7b068ea6006754f8363caa5f9876131c0a
Klyutkina <Elena.Klyutkina@x5.ru>
2022-07-17 14:49:52 +0300

    006

---
+++ 006+/Program.cs
@@ -0,0 +1,37 @@
+﻿// Написать программу вычисления значения функции y = sin(a). (Класс Math)
+
+double x,y;
+// x=3.14;
+x=Math.PI;
+y=Math.Sin(x);
+System.Console.WriteLine($"sin({x})={y}");
+
+/*
+//тип возвращаемого значения Имя_функции (аргументы функции)
+double Sin(double x) // методы
+{
+    return Math.Sin(x);
+}
+
+
+double Sum(double a,double b) // методы
+{
+    return a+b;
+}
+
+double Sum(int a,int b) // методы
+{
+    return a+b;
+}
+
+
+void Pause() //метод. void означает, что функция ничего не возвращает
+{
+    System.Console.WriteLine("Press any key");
+}
+
+
+double y=Sin(3.14);
+System.Console.WriteLine(y);
+Pause();
+*/
