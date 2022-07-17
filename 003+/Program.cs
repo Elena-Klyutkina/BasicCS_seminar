ccc2a102f1850d932918bb2c5f8547b157fdb0e1
Klyutkina <Elena.Klyutkina@x5.ru>
2022-07-17 14:39:29 +0300

    003

---
+++ 003+/Program.cs
@@ -0,0 +1,28 @@
+﻿// С клавиатуры вводятся два числа a и b. Найти максимальное из них.
+
+
+// Ввод целых чисел
+int a,b;
+string? c=Console.ReadLine();
+string? d=Console.ReadLine();
+a=Convert.ToInt32(c);
+b=Convert.ToInt32(d);
+
+if (a>b) {
+    System.Console.WriteLine("Максимальное число "+$"{a}");
+} else {
+    System.Console.WriteLine("Максимальное число "+$"{b}");
+}
+
+// Ввод дробных чисел
+double a1,b1;
+string? c1=Console.ReadLine();
+string? d1=Console.ReadLine();
+a1=Convert.ToDouble(c1);
+b1=Convert.ToDouble(d1);
+
+if (a1>b1) {
+    System.Console.WriteLine("Максимальное число "+$"{a1}");
+} else {
+    System.Console.WriteLine("Максимальное число "+$"{b1}");
+}
