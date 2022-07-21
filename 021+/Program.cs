9933a8d8ad1f384bd9d362d8b00575bb118054b3
Klyutkina <Elena.Klyutkina@x5.ru>
2022-07-21 22:50:39 +0300

    021

---
+++ 021+/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задать номер четверти, показать диапазоны для возможных координат
+
+System.Console.WriteLine("Введите номер координатной четверти: ");
+int quarterNumber = Convert.ToInt32(System.Console.ReadLine());
+if (quarterNumber==2 || quarterNumber==3) {
+    System.Console.WriteLine("x<0");
+}
+if (quarterNumber==1 || quarterNumber==4) {
+    System.Console.WriteLine("x>0");
+}
+if (quarterNumber==1 || quarterNumber==2) {
+    System.Console.WriteLine("y>0");
+}
+if (quarterNumber==3 || quarterNumber==4) {
+    System.Console.WriteLine("y<0");
+} else {
+System.Console.WriteLine("Введите число от 1 до 4");
+}
