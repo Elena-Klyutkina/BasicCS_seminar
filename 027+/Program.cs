4be28c1232e0efa88de6f739d7e70dc5211f1168
Klyutkina <Elena.Klyutkina@x5.ru>
2022-07-22 00:24:37 +0300

    027

---
+++ 027+/Program.cs
@@ -0,0 +1,11 @@
+﻿// Возведите число А в натуральную степень B, используя цикл
+
+System.Console.Write("Введите число: ");
+int A=Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите степень, в которую нужно возвести указанное выше число: ");
+int B=Convert.ToInt32(Console.ReadLine());
+int numberInPower=1;
+for(int i = 1; i <= B; i++){
+    numberInPower = numberInPower*A;
+};
+Console.WriteLine(numberInPower);
