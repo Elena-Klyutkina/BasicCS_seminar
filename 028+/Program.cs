81ab2c062b64976145676b96b285a963295e8a6a
Klyutkina <Elena.Klyutkina@x5.ru>
2022-07-21 20:25:49 +0300

    028

---
+++ 028+/Program.cs
@@ -0,0 +1,14 @@
+﻿// Определить количество цифр в числе. Сделать подпрограмму.
+
+int CountDigits(int N)
+{
+    int k=0;
+    while(N!=0)
+    {
+        k++;
+        N=N/10;
+    }
+    return k;
+} 
+
+System.Console.WriteLine(CountDigits(125));
