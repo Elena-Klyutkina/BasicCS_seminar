4b65e6d09ae419b3794a53f62577fbc58e75a388
Klyutkina <Elena.Klyutkina@x5.ru>
2022-07-21 20:09:52 +0300

    007

---
+++ 007+/Program.cs
@@ -0,0 +1,10 @@
+﻿// Выяснить является ли число чётным.
+
+Console.Write("Введите число: ");
+string? s=Console.ReadLine();
+int a=Convert.ToInt32(s);
+if (a%2==0){
+    System.Console.WriteLine("Число четное");
+} else {
+    System.Console.WriteLine("Число нечетное");
+}
