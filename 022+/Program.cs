acd6fe39544a0e7c775259c8ee5c854d1568b3b2
Klyutkina <Elena.Klyutkina@x5.ru>
2022-07-21 22:25:59 +0300

    021-023

---
+++ 022+/Program.cs
@@ -0,0 +1,22 @@
+﻿// Программа проверяет пятизначное число на палиндромом.
+
+System.Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int temp = number;
+int numberReversed = 0;
+
+while (number > 0)
+{
+    var n = number % 10;
+    numberReversed = numberReversed * 10 + n;
+    number = number/10;
+}
+
+System.Console.WriteLine($"{numberReversed}");
+
+if(temp==numberReversed){
+    System.Console.WriteLine("Число является палиндромом");
+}
+else {
+    System.Console.WriteLine("Число не является палиндромом");
+}
---
+++ 023+/Program.cs
@@ -0,0 +1,12 @@
+﻿// Найти расстояние между точками в пространстве 2D
+
+System.Console.Write("x1=");
+Double x1 = Convert.ToDouble(Console.ReadLine());
+System.Console.Write("x2=");
+Double x2 = Convert.ToDouble(Console.ReadLine());
+System.Console.Write("y1=");
+Double y1 = Convert.ToDouble(Console.ReadLine());
+System.Console.Write("y2=");
+Double y2 = Convert.ToDouble(Console.ReadLine());
+Double distance = Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2));
+Console.WriteLine("Расстояние между точками в проастранстве равно " + Math.Round(distance,2));
