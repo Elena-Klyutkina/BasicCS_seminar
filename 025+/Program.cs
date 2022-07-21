ba6e050637b775e06f75aa69bf2c015e3f711ee2
Klyutkina <Elena.Klyutkina@x5.ru>
2022-07-21 23:52:11 +0300

    024-025

---
+++ 025+/Program.cs
@@ -0,0 +1,9 @@
+﻿// Вывести на экран кубы чисел от 1 до N
+
+System.Console.Write("Введите число: ");
+int N=Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Число" + "\t" + "Куб числа");
+
+for(int i = 1; i <= N; i++)
+Console.WriteLine(i + "\t" + i*i*i);
