b8cbec0a04e58425a956f12af2c83b4f51f497d1
Klyutkina <Elena.Klyutkina@x5.ru>
2022-07-22 00:46:13 +0300

    029-030

---
+++ 029+/Program.cs
@@ -0,0 +1,12 @@
+﻿// Подсчитать сумму цифр в числе
+
+System.Console.Write("Введите число: ");
+int a=Convert.ToInt32(Console.ReadLine());
+int sum=0;
+
+while (a>0)
+{
+    sum = sum + a%10;
+    a = a/10;
+}
+ Console.WriteLine(sum);
---
+++ 030+/Program.cs
@@ -0,0 +1,11 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+
+System.Console.Write("Введите число: ");
+int N=Convert.ToInt32(Console.ReadLine());
+int mult=1;
+
+for (int i=1; i<=N; i++)
+{
+    mult = mult*i;
+}
+System.Console.WriteLine(mult);
