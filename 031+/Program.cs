0ecb71b98cdcf6348401eaeb7d0fb9b1ebcb3d9a
Klyutkina <Elena.Klyutkina@x5.ru>
2022-07-22 01:06:26 +0300

    031

---
+++ 031+/Program.cs
@@ -0,0 +1,15 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+
+
+System.Console.Write("Введите число: ");
+int N=Convert.ToInt32(Console.ReadLine());
+int cube=1;
+if(N%2==0 && N!=0){
+    for(int i=0; i<=N; i++){
+        cube=i*i*i;
+    }
+    Console.WriteLine("Куб числа равен " + cube);
+}
+else {
+    System.Console.WriteLine("Введите четное число");
+}
