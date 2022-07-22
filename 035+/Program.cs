526ecddf884205a779e3436e503973f409530b0e
Klyutkina <Elena.Klyutkina@x5.ru>
2022-07-22 21:14:26 +0300

    033,035,036

---
+++ 035+/Program.cs
@@ -0,0 +1,37 @@
+﻿// Написать программу замены элементов массива на противоположные
+
+System.Console.Write("Введите размерность массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[] t;
+Init(out t,n,1,100);
+Print(t);
+Change(t);
+PrintNegativ(t);
+
+void Init(out int[] t, int Length,int min,int max)
+{
+  t=new int[Length];
+  Random random=new Random();
+  for(int i=0;i<t.Length;i++) 
+    t[i]=random.Next(min,max+1);
+}
+
+void Print(int[] t)
+{
+  for(int i=0;i<t.Length;i++)
+  System.Console.Write($"{t[i],7}");
+  System.Console.WriteLine();
+}
+
+void Change(int[] t)
+{
+    for (int i=0;i<t.Length;i++)
+    if (t[i]>0) t[i] = -t[i];
+}
+
+void PrintNegativ(int[] t)
+{
+  for(int i=0;i<t.Length;i++)
+  System.Console.Write($"{t[i],7}");
+}
