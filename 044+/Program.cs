e2ac13f9ecb11112e77eb07f812cb71ca73e705e
Klyutkina <Elena.Klyutkina@x5.ru>
2022-07-23 13:38:37 +0300

    044

---
+++ 044+/Program.cs
@@ -0,0 +1,16 @@
+﻿//Написать программу преобразования десятичного числа в двоичное
+
+int n=555;
+System.Console.WriteLine(DecToBin(n));
+
+string DecToBin(int n)
+{
+    if (n==0) return "0";
+    string s="";
+    while (n!=0)
+    {
+        s=(n%2).ToString()+s;    
+        n/=2;//n=n/2
+    }
+    return s;
+}
