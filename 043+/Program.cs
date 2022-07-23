5f32e91302344be4441ff1863a110b1f0a223c85
Klyutkina <Elena.Klyutkina@x5.ru>
2022-07-23 13:33:05 +0300

    043

---
+++ 043+/Program.cs
@@ -0,0 +1,25 @@
+﻿// Определить сколько чисел больше 0 введено с клавиатуры
+
+Main();
+
+void Main()
+    {
+        try
+        {
+            Console.Write("Введите элементы(через пробел): ");
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int count = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > 0)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine($"Кол-во элементов больше 0: {count}");
+        }
+        catch (SystemException)
+        {
+             Console.WriteLine("Во время выполнения программы произошла ошибка, вероятно, были введены некорректные данные!");
+        }
+    }
