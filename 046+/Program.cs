4abdc2ec8b699f77c41222e30da91408fe09f55c
Klyutkina <Elena.Klyutkina@x5.ru>
2022-07-23 14:39:59 +0300

    046

---
+++ 046+/Program.cs
@@ -0,0 +1,20 @@
+﻿// Показать числа Фибоначчи
+
+Console.Write("Введите конец диапазона: ");
+int end=int.Parse(Console.ReadLine());
+int prev = 0;
+int current = 1;
+int temp;
+Console.Write($"{prev,4}");
+Main();
+
+void Main()
+    {
+        for (int i = 0; i <= 10; i++)
+        {
+            Console.Write($"{current,4}");
+            temp = current;
+            current = current + prev;
+            prev = temp;
+        }
+    }
