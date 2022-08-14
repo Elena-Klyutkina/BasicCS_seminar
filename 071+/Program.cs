f380e3012d1a63a9b9b0bec3763b6321acfe9114
Klyutkina <Elena.Klyutkina@x5.ru>
2022-08-14 15:02:20 +0300

    071

---
+++ 071+/Program.cs
@@ -0,0 +1,10 @@
+﻿// Написать программу вычисления функции Аккермана
+
+static int A(int n, int m)
+{
+    if (m == 0) return n + 1;
+    if (m != 0 && n == 0) return A(m - 1, 1);
+    else return A(m - 1, A(m, n - 1));
+}
+
+System.Console.WriteLine(A(1, 2));
