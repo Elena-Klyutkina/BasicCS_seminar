afc1af65cbc872bdd0410c25314905e83e3ec480
Klyutkina <Elena.Klyutkina@x5.ru>
2022-08-14 14:41:49 +0300

    073

---
+++ 073+/Program.cs
@@ -0,0 +1,17 @@
+﻿// 73 Написать программу показывающие первые N чисел, для которых каждое следующее равно сумме двух предыдущих. Первые два элемента последовательности 0 и 1
+
+int n;
+Console.Write("Введите число N: ");
+string? s=Console.ReadLine();
+n=Convert.ToInt32(s);
+int fibonacchi(int n)
+
+{
+    if (n==1 || n==2) return 1;
+    else return fibonacchi(n-1) + fibonacchi(n-2);
+}
+
+for (int i=1; i<=n; i++)
+{
+    System.Console.WriteLine(fibonacchi(i));
+}
